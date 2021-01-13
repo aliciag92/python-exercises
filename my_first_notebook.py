677b4264c15bb236a171aa5f51bf738e262919f2
Alicia Gonzalez <alicia.j.gonzalez01@gmail.com>
2021-01-12 20:00:24 -0600

    first Jupyter notebook

---
+++ my_first_notebook.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+2 + 2
+
+
+# In[2]:
+
+
+2+3
+
+
+# In[3]:
+
+
+2+4
+
+
+# # Jupyter Notebook
+# ## shortcuts
+# 
+# - jupyter notebook: runs the jupyter notebook server in your pwd
+# - jupyter notebook notebook_name.ipynb: runs a specific notebook on the server
+# 
+# - shift + enter: runs cells of code/markdown
+#     
+
+# In[ ]:
+
+
+
+
