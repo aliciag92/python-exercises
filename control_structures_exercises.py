68b8f516f6ebdcb639476da8de08b6e7c4f86436
Alicia Gonzalez <alicia.j.gonzalez01@gmail.com>
2021-01-14 11:36:27 -0600

    completed exercises

--- control_structures_exercises.py
+++ control_structures_exercises.py
@@ -11,6 +11,9 @@ if day_of_the_week == 'Monday':
 else:
     print('It is not Monday')
 
+#another solution
+
+
 
 #1b. prompt the user for a day of the week, print out whether the day is a weekday or a weekend
 weekday = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
@@ -20,6 +23,11 @@ if day_of_the_week in weekday:
     print ("It is a weekday")
 else: 
     print ("It is the weekend")
+
+#another solution
+
+
+
 
 #1c. create variables and make up values for
     #the number of hours worked in one week
@@ -40,6 +48,10 @@ if number_of_hours_worked > 40:
 else:
     weekly_pay = number_of_hours_worked * hourly_rate
     print(weekly_pay)
+
+#another solution:
+
+
 
 #2. Loop Basics
 #2a. While
@@ -85,7 +97,7 @@ else:
     i = 2
     while i < 1000000:
         print (i)
-        i **= 2 #or i = i ** 2
+        i **= 2 #or i = i ** 2 or i *= i
     #Output should equal:
         # 2
         # 4
@@ -153,16 +165,25 @@ else:
         # 999999999
 
 #2c. break and continue
-    ##2ci. Prompt the user for an odd number between 1 and 50. Use a loop and a break statement to continue prompting the user if they enter invalid input. (Hint: use the isdigit method on strings to determine this). Use a loop and the continue statement to output all the odd numbers between 1 and 50, except for the number the user entered.
-    odd_number_chosen = int(input ("Pick an odd number between 1 and 50: "))
-    for number in range (1, 51):
-        if number = odd_number_chosen:
-            print (f "Yikes! Skipping number: {number}")
-            break
+    ##2ci. Prompt the user for an odd number between 1 and 50. 
+    # Use a loop and a break statement to continue prompting the user if they enter invalid input. (Hint: use the isdigit method on strings to determine this). 
+    # Use a loop and the continue statement to output all the odd numbers between 1 and 50, except for the number the user entered.
+while True:
+    user_number = input("Please enter an odd number between 1 and 50")
+    if user_number.isdigit():
+        user_number = int(user_number)
+        if user_number % 2 == 0: # if it's even
             continue
-        if number % 2 != 0:
-            print(f "Here is an odd number: {number}")
+        break
 
+i = 1
+while i <= 50:
+    if i == user_number:
+        print(f"Yikes! Skipping number: {i}")
+        i += 2
+        continue
+    print(f"Here is an odd number: {i}")
+    i += 2
             #Your output should look like this:
                 # Number to skip is: 27
 
@@ -191,19 +212,47 @@ else:
                 # Here is an odd number: 45
                 # Here is an odd number: 47
                 # Here is an odd number: 49
+
+#Walkthrough:
+#continuously
+    # prompt the user for a positive number
+    # Check if the input is composed of digits, if so
+        # Convert to a numeric type
+        # Check if the number is negative or 0, if so
+        # Go back to square one
+    # Stop the loop (break)
+
+
 #2d. The input function can be used to prompt for input and use that input in your python code. 
     # Prompt the user to enter a positive number and write a loop that counts from 0 to that number. 
     # (Hints: first make sure that the value the user entered is a valid number, also note that the input function returns a string, so you'll need to convert this to a numeric type.)
-positive_number = int(input ("Enter a positive number: "))
-i = 0
-    while i <= positive_number:
-        print (i)
-        i += 1 
+# enter a positive number
+while True:
+    user_number = input("Please enter a positive number: ")
+    if user_number.isdigit():
+        user_number = int(user_number)
+        if user_number <= 0:
+            continue
+        break
 
+for i in range(0, user_number + 1):
+    print(i)
 
 
 #2e. Write a program that prompts the user for a positive integer. 
 # Next write a loop that prints out the numbers from the number the user entered down to 1.
+# enter a positive number
+while True:
+    user_number = input("Please enter a positive number: ")
+    if user_number.isdigit():
+        user_number = int(user_number)
+        if user_number <= 0:
+            continue
+        break
+
+for i in range(user_number, 0, -1):     #list(range(10, 0, -1))
+    print(i)
+
 
 #3. Fizzbuzz
 #One of the most common interview questions for entry-level programmers is the FizzBuzz test. Developed by Imran Ghory, the test is designed to test basic looping and conditional logic skills.
@@ -211,6 +260,16 @@ i = 0
     #For multiples of three print "Fizz" instead of the number
     #For the multiples of five print "Buzz".
     #For numbers which are multiples of both three and five print "FizzBuzz".
+for i in range(1, 101):
+    if i % 5 == 0 and i % 3 == 0:
+        print('FizzBuzz')
+    elif i % 3 == 0:
+        print('Fizz')
+    elif i % 5 == 0:
+        print('Buzz')
+    else:
+        print(i)
+
 
 #4. Display a table of powers.
     #Prompt the user to enter an integer.
@@ -218,6 +277,14 @@ i = 0
     #Ask if the user wants to continue.
     #Assume that the user will enter valid data.
     #Only continue if the user agrees to.
+user_input = int(input("Please enter an integer: "))
+
+print()
+print("number | squared | cubed")
+print("------ | ------- | -----")
+for i in range(1, user_input + 1):
+    print("%6d | %7d | %5d" % (i, i**2, i**3))
+
     #Example Output
         #What number would you like to go up to? 5
 
@@ -233,6 +300,8 @@ i = 0
 
 #Bonus: Research python's format string specifiers to align the table
 
+
+
 #5. Convert given number grades into letter grades.
     #Prompt the user for a numerical grade from 0 to 100.
     #Display the corresponding letter grade.
@@ -246,8 +315,70 @@ i = 0
         #D : 66 - 60
         #F : 59 - 0
 
+numeric_grade = int(input("Please enter a number grade: "))
+
+# convert to letter
+if numeric_grade >= 88:
+    print("A")
+elif numeric_grade >= 80:
+    print("B")
+elif numeric_grade >= 67:
+    print("C")
+elif numeric_grade >= 60:
+    print("D")
+else:
+    print("F")
+
+#with while loop:
+while True:
+    # TODO: prompt user for this value
+    numeric_grade = int(input("Please enter a number grade: "))
+
+    # convert to letter
+    if numeric_grade >= 88:
+        print("A")
+    elif numeric_grade >= 80:
+        print("B")
+    elif numeric_grade >= 67:
+        print("C")
+    elif numeric_grade >= 60:
+        print("D")
+    else:
+        print("F")
+    
+    wants_to_continue = input("Do you want to continue? ")
+    if not wants_to_continue.lower().startswith("y"):
+        break
+
 #Bonus
 #Edit your grade ranges to include pluses and minuses (ex: 99-100 = A+).
 
-#6. Create a list of dictionaries where each dictionary represents a book that you have read. Each dictionary in the list should have the keys title, author, and genre. Loop through the list and print out information about each book.
+#6. Create a list of dictionaries where each dictionary represents a book that you have read. 
+# Each dictionary in the list should have the keys title, author, and genre. Loop through the list and print out information about each book.
 #6a. Prompt the user to enter a genre, then loop through your books list and print out the titles of all the books in that genre.
+# consider that this data structure could represent rows from a
+# database table
+books = [
+    {"title": "This is where I leave you", "author": "Jonathan Tropper", "genre": "Adult Fiction"},
+    {'title': 'Parable of the Sower', 'author': 'Octavia E. Butler', 'genre': 'Science Fiction'},
+    {"title": "Dune", "author": "Frank Herbert", "genre": "Science Fiction"},
+    {'title': 'The Singularity is Near', 'author': 'Ray Kurzweil', 'genre': 'Artificial intelligence/Technology'},
+    {"title": "Star Wars: The Old Republic - Revan", "author": "Sean Williams", "genre": "Science Fiction"},
+]
+
+selected_genre = input("Please enter a genre: ")
+selected_books = [book for book in books if book['genre'] == selected_genre]
+
+# for singular in plural
+# plural variable names indicate a list of things
+# the singular version is one thing from the list
+for book in selected_books:
+    print("---")
+    # print("title: {}".format(book['title']))
+    # print("author: {}".format(book['author']))
+    # print("genre: {}".format(book['genre']))
+    
+    # other ways to do it
+    print("Title: %s" % book['title'])
+    print("Author: " + book['author'])
+    print(f"Genre: {book['genre']}")
