b114d0ab2effb283447ec2fbc1fa4cebccc7d05d
Alicia Gonzalez <alicia.j.gonzalez01@gmail.com>
2021-01-16 18:21:42 -0600

    Completed import exercises

--- import_exercises.py
+++ import_exercises.py
@@ -29,20 +29,84 @@ len(list(it.combinations('abcd', 2))) #6
 import json
 profiles = json.load(open('profiles.json'))
     # Total number of users
-    len(profiles) #19
+len(profiles) #19
     # Number of active users
-    len([profile for profile in profiles if profile['isActive']]) #9
+len([profile for profile in profiles if profile['isActive']]) #9
     # Number of inactive users
-    len([profile for profile in profiles if not profile['isActive']]) #10
+len([profile for profile in profiles if not profile['isActive']]) #10
         #or count = 0
         #   for user in profiles:
         #       if user['isActive'] == True:
         #           count = count + 1
         #   print(count)cs
+        #or
+        # len([profile for profile in profiles if profile['isActive'] == False])
     # Grand total of balances for all users
+def convert_balances(balances):
+    balances_float = []
+    
+    for balance in balances:
+        balance = balance.replace("$", "")
+        balance = balance.replace(",", "")
+        
+        balances_float.append(float(balance))
+    
+    return balances_float
+
+balance = convert_balances([profile["balance"] for profile in profiles])
+total_balance = sum(balance)
+
+print(total_balance) #52667.02
     # Average balance per user
+average_balance = total_balance / len(profiles)
+
+round(average_balance, 2) #2771.95
     # User with the lowest balance
+def convert_balances(balances):
+    balances_float = []
+    
+    for balance in balances:
+        balance = balance.replace("$", "")
+        balance = balance.replace(",", "")
+        
+        balances_float.append(float(balance))
+    
+    return balances_float
+
+balance = convert_balances([profile["balance"] for profile in profiles])
+lowest_balance = min(balance)
+
+print(lowest_balance) #1214.1
     # User with the highest balance
+def convert_balances(balances):
+    balances_float = []
+    
+    for balance in balances:
+        balance = balance.replace("$", "")
+        balance = balance.replace(",", "")
+        
+        balances_float.append(float(balance))
+    
+    return balances_float
+
+balance = convert_balances([profile["balance"] for profile in profiles])
+highest_balance = max(balance)
+
+print(highest_balance) #3919.64
     # Most common favorite fruit
+import statistics
+
+statistics.mode(profile["favoriteFruit"] for profile in profiles) #'strawberry
     # Least most common favorite fruit
+min(profile["favoriteFruit"] for profile in profiles) #'apple'
     # Total number of unread messages for all users
+greetings = [profile["greeting"] for profile in profiles]
+unread_messages = []
+
+for greeting in greetings:
+    for word in greeting:
+        if word.isnumeric():
+            unread_messages.append(int(word))
+
+total_unread_messages = sum(unread_messages)
+print(total_unread_messages) #111
